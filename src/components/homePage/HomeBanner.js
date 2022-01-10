2a66bb88182e930cfc47a4bf794d087e33144abf
Menat Pink <pink@Pinks-MacBook-Pro.local>
2022-01-10 14:35:41 +0000

    Temporarily disable canvas

--- src/components/homePage/HomeBanner.js
+++ src/components/homePage/HomeBanner.js
@@ -16,59 +16,60 @@ const HomeBanner = ({ onCursor }) => {
   const size = useWindowSize()
   const { currentTheme } = useGlobalStateContext()
   let canvas = useRef(null)
-  useEffect(() => {
-    let renderingElement = canvas.current
 
-    //create an offscreen canvas only for the drawings
-    let drawingElement = renderingElement.cloneNode()
-    let drawingCtx = drawingElement.getContext("2d")
-    let renderingCtx = renderingElement.getContext("2d")
-    let lastX
-    let lastY
-    let moving = false
+  // useEffect(() => {
+  //   let renderingElement = canvas.current
 
-    renderingCtx.globalCompositeOperation = "source-over"
-    renderingCtx.fillStyle = currentTheme === "dark" ? "#000000" : "#ffffff"
-    renderingCtx.fillRect(0, 0, size.width, size.height)
+  // // create an offscreen canvas only for the drawings
+  // let drawingElement = renderingElement.cloneNode()
+  // let drawingCtx = drawingElement.getContext("2d")
+  // let renderingCtx = renderingElement.getContext("2d")
+  // let lastX
+  // let lastY
+  // let moving = false
 
-    renderingElement.addEventListener("mouseover", ev => {
-      moving = true
-      lastX = ev.pageX - renderingElement.offsetLeft
-      lastY = ev.pageY - renderingElement.offsetTop
-    })
+  //   renderingCtx.globalCompositeOperation = "source-over"
+  //   renderingCtx.fillStyle = currentTheme === "dark" ? "#000000" : "#ffffff"
+  //   renderingCtx.fillRect(0, 0, size.width, size.height)
 
-    renderingElement.addEventListener("click", ev => {
-      moving = true
-      lastX = ev.pageX - renderingElement.offsetLeft
-      lastY = ev.pageY - renderingElement.offsetTop
-    })
+  //   renderingElement.addEventListener("mouseover", ev => {
+  //     moving = true
+  //     lastX = ev.pageX - renderingElement.offsetLeft
+  //     lastY = ev.pageY - renderingElement.offsetTop
+  //   })
 
-    renderingElement.addEventListener("mouseup", ev => {
-      moving = false
-      lastX = ev.pageX - renderingElement.offsetLeft
-      lastY = ev.pageY - renderingElement.offsetTop
-    })
+  //   renderingElement.addEventListener("click", ev => {
+  //     moving = true
+  //     lastX = ev.pageX - renderingElement.offsetLeft
+  //     lastY = ev.pageY - renderingElement.offsetTop
+  //   })
 
-    renderingElement.addEventListener("mousemove", ev => {
-      if (moving) {
-        drawingCtx.globalCompositeOperation = "source-over"
-        renderingCtx.globalCompositeOperation = "destination-out"
-        let currentX = ev.pageX - renderingElement.offsetLeft
-        let currentY = ev.pageY - renderingElement.offsetTop
-        drawingCtx.lineJoin = "round"
-        drawingCtx.moveTo(lastX, lastY)
-        drawingCtx.lineTo(currentX, currentY)
-        drawingCtx.closePath()
-        drawingCtx.lineWidth = 100
-        drawingCtx.stroke()
-        lastX = currentX
-        lastY = currentY
-        renderingCtx.drawImage(drawingElement, 0, 0)
-        console.log(`currentX is ${currentX}`)
-        console.log(`currentY is ${currentY}`)
-      }
-    })
-  }, [currentTheme, size.height, size.width])
+  //   renderingElement.addEventListener("mouseup", ev => {
+  //     moving = false
+  //     lastX = ev.pageX - renderingElement.offsetLeft
+  //     lastY = ev.pageY - renderingElement.offsetTop
+  //   })
+
+  //   renderingElement.addEventListener("mousemove", ev => {
+  //     if (moving) {
+  //       drawingCtx.globalCompositeOperation = "source-over"
+  //       renderingCtx.globalCompositeOperation = "destination-out"
+  //       let currentX = ev.pageX - renderingElement.offsetLeft
+  //       let currentY = ev.pageY - renderingElement.offsetTop
+  //       drawingCtx.lineJoin = "round"
+  //       drawingCtx.moveTo(lastX, lastY)
+  //       drawingCtx.lineTo(currentX, currentY)
+  //       drawingCtx.closePath()
+  //       drawingCtx.lineWidth = 100
+  //       drawingCtx.stroke()
+  //       lastX = currentX
+  //       lastY = currentY
+  //       renderingCtx.drawImage(drawingElement, 0, 0)
+  //       console.log(`currentX is ${currentX}`)
+  //       console.log(`currentY is ${currentY}`)
+  //     }
+  //   })
+  // }, [currentTheme, size.height, size.width])
 
   const parent = {
     initial: { y: 800 },
@@ -103,13 +104,13 @@ const HomeBanner = ({ onCursor }) => {
           src={require("../../assets/video/video.mp4")}
         />
       </Video>
-      <Canvas
+      {/* <Canvas
         height={size.height}
         width={size.width}
         ref={canvas}
         onMouseEnter={() => onCursor("hovered")}
         onMouseLeave={onCursor}
-      />
+      /> */}
       <BannerTitle variants={parent} initial="initial" animate="animate">
         <Headline variants={child}>DIG</Headline>
         <Headline variants={child}>DEEP</Headline>
